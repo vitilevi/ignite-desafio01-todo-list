eb4f175692b75ce5b7fdcaac3856961246818b95
Victor Faria <vitilevi@hotmail.com>
2023-02-21 16:57:06 -0300

    Complete task styling.

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,7 +1,7 @@
 import style from '../styles/task.module.css';
 import { ChangeEvent } from 'react';
 import { TaskInterface } from '../types';
-import { Trash } from 'phosphor-react';
+import { CheckCircle, Circle, Trash } from 'phosphor-react';
 
 export interface TaskProps {
   task: TaskInterface;
@@ -17,23 +17,34 @@ export default function Task({task, removeTask, changeTaskStatus}: TaskProps) {
     );
   };
 
+  const getCheckBox = () => {
+    const iconSize = 24;
+    if (task.isCompleted) {
+      return <CheckCircle className="checked" size={iconSize} color="#5E60CE" weight="fill" />;
+      }
+    return <Circle className="circle" size={iconSize} color="#4EA8DE" />;
+  }
+
   return (
     <div className={style.taskWrapper}>
-      <input
-        type="checkbox"
-        checked={task.isCompleted}
-        onChange={handleInputChange}
-      />
-      <div>
-        <p>{task.title}</p>
-      </div>
+      <label>
+        <input
+          type="checkbox"
+          checked={task.isCompleted}
+          onChange={handleInputChange}
+        />
+        { getCheckBox() }
+        <div>
+          <p>{task.title}</p>
+        </div>
+      </label>
       <button
         type='button'
         aria-label="Remover tarefa"
         onClick={() => removeTask(task.id)}
       >
         <Trash
-          size={16}
+          size={18}
           color="#808080"
         />
       </button>
